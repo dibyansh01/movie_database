ca2822543565f1f9d96ea0cac2fa412fc41e4430
Dibyanshu <dibyansh01iitk@gmail.com>
2023-09-05 03:49:26 +0530

    initial commit

--- database.py
+++ database.py
@@ -8,25 +8,25 @@ db_config = {
     "password": "pandey",
 }
 
-# Open the JSON file and load data
+# Opening the JSON file and load data
 with open('imdb.json', 'r') as file:
     data = json.load(file)
 
-# Connect to the MySQL server
+# Connecting to the MySQL server
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor()
 
-# Create the 'imdb_movies' database if it doesn't exist
+# Creating the 'imdb_movies' database if it doesn't exist
 create_db_query = "CREATE DATABASE IF NOT EXISTS imdb_movies;"
 cursor.execute(create_db_query)
 conn.commit()
 
-# Use the 'imdb_movies' database
+# Using the 'imdb_movies' database
 db_config["database"] = "imdb_movies"
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor()
 
-# Create the 'movies' table with the 'genres' column as VARCHAR
+# Creating the 'movies' table with the 'genres' column as VARCHAR
 create_table_query = """
 CREATE TABLE IF NOT EXISTS movies (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS movies (
 
 cursor.execute(create_table_query)
 
-# Insert data into the 'movies' table
+# Inserting data into the 'movies' table
 for movie in data:
     insert_query = """
     INSERT INTO movies (99popularity, director, imdb_score, name, genres)
@@ -49,7 +49,7 @@ for movie in data:
     values = (movie["99popularity"], movie["director"], movie["imdb_score"], movie["name"], json.dumps(movie["genre"]))
     cursor.execute(insert_query, values)
 
-# Commit changes and close the connection
+# Committing changes and close the connection
 conn.commit()
 conn.close()
 
